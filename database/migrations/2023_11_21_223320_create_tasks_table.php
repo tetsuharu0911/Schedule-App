7b4e5d39fde15b97e26645a0a4fb77cb0765c012
tetsuharu0911 <tetsuharu.nasu@gmail.com>
2023-11-23 09:19:06 +0900

    Initial commit

---
+++ database/migrations/2023_11_21_223320_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('task_name');
+            $table->table('description')->nullable();
+            $table->unsignedSmallInteger('status')->default(0);     // 0:未実施、1:実施中、2:完了
+            $table->dateTime('due_date')->nullable();
+            // $table->unsignedBigInteger('user_id');
+            // $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
